9408487d01f8996b999e4ffa555861a6a5514d18
jenlockett <jenlockett@users.noreply.github.com>
2016-02-07 14:51:38 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pita Pushers Order</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+
+
+<!--------------------Header---------------------->
+<header>
+    <section class="secOne">
+        
+        
+    </section>
+</header>
+
+
+<!---------------------------Main------------------------->
+<section class="secTwo">
+
+
+    <h1>Pita Pushers Online Ordering</h1>
+        <p>Order online and pick up at the truck!</p>
+        
+        <form action="">
+            <p>What are you going to order?</p>
+            <input type="radio" name="item" value="sandwich" > Falafel<br>
+            <input type="radio" name="item" value="" > <br>
+            <input type="radio" name="item" value="" > <br>
+        </form>
+        
+        <br>
+        <br>
+        <br>
+        
+      <tr>      
+        <td>
+                    How Many?																						
+        </td>
+            <br>
+
+        <td>
+                <div id="button" data-role="button"></div>
+<script>
+   var buttonElement = document.getElementById("button"),
+       button = tau.widget.Button(buttonElement);
+       button.value("text");
+</script>
+         </td>
+      </tr>
+        
+<hr>
+        
+        <form action="">
+            <p>Do you want fries with that?</p>
+            
+            <input type="checkbox" name="side" value="fries" > Harra Frites<br>
+            <input type="checkbox" name="side" value="" > <br>
+            <input type="checkbox" name="side" value="" > <br>
+               
+            <input type="checkbox" name="side" value="" > <br>
+            <input type="checkbox" name="side" value="" > <br>
+            <input type="checkbox" name="side" value="" > <br>
+            
+            <br>
+            
+            <button class="submit" type="submit" name="submit" value="add">Add to Order</button>
+        </form>
+
+</section>
+        
+        
+<section class="secThree">
+    <h1>
+	Order Detail
+	</h1>
+</section>
+
+
+    
+
+
+<!-----Footer-------------------->
+<footer>
+    <section class="secFour">
+        
+        
+    </section>
+</footer>
+
+    </div>
+</body>
+</html>
